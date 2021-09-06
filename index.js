fd9f62da5f8cbaa87de71a62c28f26c59b4f4a57
estorey99 <79337310+estorey99@users.noreply.github.com>
2021-09-06 18:23:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+//Emmett Storey
+//CSCI 4602
 const validatePassword = require('./passwordvalidation'); //import the function
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-//Emmett Storey
-//CSCI 4602
-// Your code goes here!
- rl.question("Enter a password or press q to quit.\n")(callback)=>{
-var pass= window.prompt("Enter a password to validate. (or press q to quit)/n");
-while(pass !=":q"){
-if(validatePassword(pass))
-  console.log("Password is valid: " + pass);
-else
-  console.log("Password is invalid"+ pass);
-}
+ rl.question("Enter a password or press q to quit.\n",(pass)=>{
 if(pass==":q")
   rl.close();
 else 
   callback(pass);
- }
- 
- 
+   })
+ var callback= function(pass){
+  var valid=validatePassword(pass)
+  if(valid)
+  console.log("Password is valid: " + pass+"\n");
+else
+  console.log("Password is invalid: "+ pass+"\n");
+   rl.question("",(pass)=>{
+    if(pass==":q")
+      rl.close();
+    else 
+      callback(pass);
+       })}
